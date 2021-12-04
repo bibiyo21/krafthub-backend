1660d70e4df865a61df8440f0e6ec79a79cfd94d
Ibrahem Sotejo <ibrahem.sotejo@eastvantage-europe.com>
2021-12-05 03:04:29 +0800

    update database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url( env('DATABASE_URL', "postgres://dauhzpwevnlnmh:cd715eb7b544fb40900aac82b7ff622293857cf5fc8eeb790c48b4900979b44c@ec2-44-193-111-218.compute-1.amazonaws.com:5432/d10lqcshievtbu"));
+
 return [
 
     /*
@@ -46,11 +48,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

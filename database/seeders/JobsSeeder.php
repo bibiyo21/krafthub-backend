e4afdf0362c1bff8b779633e450437938404abf7
Ibrahem Sotejo <ibrahem.sotejo@eastvantage-europe.com>
2021-12-05 16:29:03 +0800

    update seeders

--- database/seeders/JobsSeeder.php
+++ database/seeders/JobsSeeder.php
@@ -15,26 +15,31 @@ class JobsSeeder extends Seeder
     public function run()
     {
         DB::table('jobs')->insert([
+            "id" => 5,
             'title' => "Rough Carpenter",
             'description' => "Service for Rough Carpenter",
             'parent_id' => 1
         ]);
         DB::table('jobs')->insert([
+            "id" => 6,
             'title' => "Joister",
             'description' => "Service for Joister",
             'parent_id' => 1
         ]);
         DB::table('jobs')->insert([
+            "id" => 7,
             'title' => "Trim Carpenter",
             'description' => "Service for Trim Carpenter",
             'parent_id' => 1
         ]);
         DB::table('jobs')->insert([
+            "id" => 8,
             'title' => "Cabinet maker",
             'description' => "Service for Cabinet maker",
             'parent_id' => 1
         ]);
         DB::table('jobs')->insert([
+            "id" => 9,
             'title' => "Framer",
             'description' => "Service for Framer",
             'parent_id' => 1

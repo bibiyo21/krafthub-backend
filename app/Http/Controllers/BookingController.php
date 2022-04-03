53ef83d1b754e9dc96ca14fb9e0f3ea5da41d776
Ibrahem Sotejo <ibrahem.sotejo@eastvantage-europe.com>
2022-04-03 16:53:07 +0800

    debug update

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -55,7 +55,7 @@ class BookingController extends Controller
         $bookings = DB::table('bookings')
             ->select( 
                 DB::raw(
-                    "bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status"
+                    "bookings.id as booking_id, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status"
                 )
             )
             ->where('bookings.user_id', '=',  auth()->user()->id)

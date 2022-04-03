26307c043d895040e9dca5c36e929a9bc99983f1
Ibrahem Sotejo <ibrahem.sotejo@eastvantage-europe.com>
2022-04-03 14:28:53 +0800

    debug update

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -39,7 +39,7 @@ class BookingController extends Controller
     public function patchBooking(Request $request) 
     {
 
-        dd($request->all(), $request->get('status'));
+        // dd($request->all(), $request->get('status'));
         $booking = Booking::find($request->get('id'));
         $booking->status = $request->get('status');
 

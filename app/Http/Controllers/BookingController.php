0fbec47d3793752742098de37f35f16e6630b544
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-04-27 20:59:55 +0800

    Update BookingController.php
    ++ amount

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -29,6 +29,7 @@ class BookingController extends Controller
             'user_id' => auth()->user()->id,
             'eta' => Carbon::parse($request->get('eta'))->format('Y-m-d H:i:s'),
             'additional_info' => $request->get('additional_info'),
+            'amount' => $request->get('amount'),
         ]);
 
         return response([
@@ -53,7 +54,7 @@ class BookingController extends Controller
         $bookings = DB::table('bookings')
             ->select( 
                 DB::raw(
-                    "bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status"
+                    "bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status, bookings.amount"
                 )
             )
             ->where('bookings.user_id', '=',  auth()->user()->id)
@@ -69,7 +70,7 @@ class BookingController extends Controller
         $bookings = DB::table('bookings')
             ->select( 
                 DB::raw(
-                    "bookings.maker_id as makerId, bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status"
+                    "bookings.maker_id as makerId, bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status, bookings.amount"
                 )
             )
             ->join('users', 'users.id', '=','bookings.maker_id');
@@ -84,7 +85,7 @@ class BookingController extends Controller
         $bookings = DB::table('bookings')
             ->select( 
                 DB::raw(
-                    "bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status"
+                    "bookings.id as bookingId, users.first_name, users.last_name, bookings.eta, bookings.additional_info, bookings.status,  bookings.amount"
                 )
             )
             ->where('bookings.maker_id', '=',  auth()->user()->id)

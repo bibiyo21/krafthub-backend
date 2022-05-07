5014cd71c719be593d1861ed38beec44d61bfe53
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-07 15:53:53 +0800

    Update 2022_05_07_072441_add_is_validated_to_users.php
    ++

--- database/migrations/2022_05_07_072441_add_is_validated_to_users.php
+++ database/migrations/2022_05_07_072441_add_is_validated_to_users.php
@@ -1,1 +1,32 @@
-Php
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsValidatedToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('isValidated')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('isValidated');
+        });
+    }
+}

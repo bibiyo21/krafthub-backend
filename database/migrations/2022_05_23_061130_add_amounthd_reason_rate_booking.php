f7ecc98b55bb75c33861332b1b08d5b45e040611
yotpa10 <52907416+yotpa10@users.noreply.github.com>
2022-05-23 19:24:48 +0800

    Create 2022_05_23_061130_add_amounthd_reason_rate_booking.php

---
+++ database/migrations/2022_05_23_061130_add_amounthd_reason_rate_booking.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAmounthdReasonRateBooking extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->decimal('amountPerHour', 5, 2)->default(0.00);
+        });
+        
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->decimal('amountPerDay', 5, 2)->default(0.00);
+        });
+
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->string('reason')->default('');
+        });
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->string('rate')->default('');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('amountPerDay');
+        });
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('amountPerHour');
+        });
+        
+
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('reason');
+        });
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('rate');
+        });
+    }
+}
